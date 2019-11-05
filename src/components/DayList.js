7cdbbbd42a2090abaf0cf0c40242afb9e2233cf1
Andrea Mastrantoni <anmastrantoni@gmail.com>
2019-11-04 22:16:46 -0500

    Fixed error of DayList component using name of daylistitem, wrote displayDaylist map function for days prop

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,5 +1,17 @@
 import React from "react";
+import DayListItem from "components/DayListItem";
 
-export default function DayListItem(props) {
-  return <ul />;
+export default function DayList(props) {
+  const { days, setDay } = props;
+
+  const displayDayList = days.map(day => (
+    <DayListItem
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={setDay}
+    />
+  ));
+
+  return <ul>{displayDayList}</ul>;
 }
